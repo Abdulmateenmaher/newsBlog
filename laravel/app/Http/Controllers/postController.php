eba2165bdf35f887220e6a2a9d3a6e3271b7ebff
AbdulmateenMaher <abdulmateenmaher23@gmail.com>
2024-01-21 11:24:14 +0430

    local project uploaded first time

---
+++ laravel/app/Http/Controllers/postController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Http\Request;
+class postController extends Controller
+{
+    //
+  public function index(){
+
+
+   $post=Post::all();
+   return response()->json([
+    "posts"=> $post
+   ]);
+
+  }
+
+  public function show($id){
+    $post=Post::where('id',$id)->withCount('comments','likes')->get();
+
+    return response()->json([
+        "post"=>$post
+    ]);
+  }
+
+  public function store(Request $request){
+
+   $request->validate([
+    "title"=>"required",
+    "description"=>"required|string"
+   ]);
+
+   $post=Post::create([
+    'title'=> $request->title,
+    'description'=> $request->description,
+    'user_id'=>auth()->user()->id
+
+   ]);
+   $post->save();
+
+   return response()->json([
+    'message' => 'post created',
+    'post description'=>$post,
+    'created with'=> User::find(1)->name,
+    'email'=> User::find(1)->email,
+   ]);
+
+  }
+
+  public function update($id , Request $request){
+
+    $post=Post::find($id);
+    $request->validate([
+        'title'=>'string',
+        'description'=>'required|string',
+    ]);
+    $post->update([
+        'title'=>$request->title,
+        'descriptiono'=>$request->description
+    ]);
+
+    return response()->json([
+     'status'=>'post updated',
+     'updated post'=>$post
+    ]);
+  }
+
+  public function destroy($id){
+    $post=Post::find($id);
+
+    $post->comments->delete();
+    $post->likes->delete();
+    $post->delete();
+    return response()->json([
+        'status'=>'post deleted'
+      ]);
+  }
+
+
+
+
+}
---
+++ laravel/app/Http/Middleware/CheckApiToken.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class CheckApiToken
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+
+    public function handle($request, Closure $next)
+{
+    if(!empty(trim($request->input('api_token')))){
+
+        $is_exists = User::where('id' , Auth::guard('api')->id())->exists();
+        if($is_exists){
+            return $next($request);
+        }
+    }
+        return response()->json('Invalid Token', 401);
+}
+}
