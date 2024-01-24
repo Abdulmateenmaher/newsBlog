00648ee28e0629acc08c216bf72f4927e9f8c01e
AbdulmateenMaher <abdulmateenmaher23@gmail.com>
2024-01-24 16:57:48 +0430

    commited full front end and back end project

---
+++ laravel/app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+
+class ApiController extends Controller
+{
+
+public function register( Request $request){
+
+    $request->validate([
+        "name" => "required",
+        "email" => "required|unique:users|email",
+        "password" => "required|confirmed",
+    ]);
+
+    User::create([
+        "name" => $request->name,
+        "email"=> $request->email,
+        "password"=> Hash::make($request->password)
+    ]);
+
+    return response()->json([
+     "status" => true,
+     "message" => "user Created Successfully"
+    ]);
+
+}
+
+public function login(Request $request){
+    $request->validate([
+     "email" => "required",
+     "password" => "required"
+    ]);
+    $user = User::where("email",$request->email)->first();
+
+    if(!empty($user)){
+        if(Hash::check($request->password, $user->password)){
+          $token= $user->createToken("myToken")->plainTextToken;
+
+          return response()->json([
+            "status" => true,
+            "message" => "login successful",
+            "token" => $token
+          ]);
+        }
+
+        return response()->json([
+         "status" => false,
+         "message" => "password doesnot match"
+        ]);
+
+    }
+
+    return response()->json([
+     "status" => false,
+     "message" => "invalid login details"
+    ]);
+
+}
+
+public function profile(){
+
+    $data=auth()->User();
+
+    return response()->json([
+        "status" => true,
+        "message" => "profile Date",
+        "data" => $data
+    ]);
+
+}
+
+public function logout(){
+
+    auth()->user()->tokens()->delete();
+
+    return response()->json([
+        "status"=> true,
+        "message" => "loged out successfully"
+    ]);
+
+}
+}
---
+++ laravel/routes/api.php
@@ -0,0 +1,47 @@
+<?php
+
+use App\Http\Controllers\Api\ApiController;
+use App\Http\Controllers\commentController;
+use App\Http\Controllers\likeController;
+use App\Http\Controllers\postController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::post('register',[ApiController::class,"register"]);
+Route::post('login',[ApiController::class,"login"]);
+
+Route::group([
+    "middleware"=>["auth:sanctum"]
+],function(){
+
+Route::get('profile',[ApiController::class,"profile"]);
+Route::get('logout',[ApiController::class,"logout"]);
+
+
+Route::get('/posts',[postController::class,'index']);
+Route::post('/posts',[postController::class,'store']);
+Route::get('posts/{id}',[postController::class,'show']);
+Route::put('posts/{id}',[postController::class,'update']);
+Route::delete('posts/{id}',[postController::class,'destroy']);
+
+
+Route::get('posts/{id}/comments',[commentController::class,'index']);
+Route::post('posts/{id}comments',[commentController::class,'store']);
+Route::put('comments/{id}',[commentController::class,'update']);
+Route::delete('comments/{id}',[commentController::class,'destroy']);
+
+Route::post('/posts/{id}/likes',[likeController::class,'create']);
+
+
+});
